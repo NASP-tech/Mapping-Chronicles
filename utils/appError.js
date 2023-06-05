70dc52da0fc62923b77458b3557f2520cdcead7b
aufreisser <aufreisser@aufreissers-MacBook-Pro.local>
2023-06-04 23:52:14 -0600

    add utils files: appError.js and catchAsync.js for custom error handling and async await functions wrapper, respectively

---
+++ utils/appError.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+    constructor(message, statusCode) {
+        super(message) // call parent constructor
+
+        this.statusCode = statusCode;
+        //operational errors, not programming errors
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor); // to preserve the stack trace for the error information retrieval
+    }
+}
+
+module.exports = AppError;
