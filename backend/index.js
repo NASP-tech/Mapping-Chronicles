08f8e155a5ed5f64a533029379518b2733897179
Steve9452 <00380119@uca.edu.sv>
2023-06-27 21:47:31 -0600

    Add : Routes to index

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,19 @@ const cors = require('cors');
 const app = express();
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
-
+const entradasUCARoute = require("./routes/entradasUCA");
+const bufferEntradasUCARoute = require("./routes/bufferEntradasUCA");
+const rutasPrimariasRoute = require("./routes/rutasPrimarias");
+const paradasPrimariasRoute = require("./routes/paradasPrimarias");
+const dinamicBufferRoute = require("./routes/dinamicBuffer");
+const layerRoute = require("./routes/layers");
 dotenv.config();
 
 app.use(express.json());
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL,
+mongoose.connect('mongodb://localhost:27017/chronicles',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -25,7 +30,11 @@ mongoose.connect(process.env.MONGO_URL,
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
-
+app.use("/api/entradasUCA", entradasUCARoute);
+app.use("/api/bufferEntradasUCA", bufferEntradasUCARoute);
+app.use("/api/rutasPrimarias", rutasPrimariasRoute);
+app.use("/api/paradasPrimarias", paradasPrimariasRoute);
+app.use("/api/layers", layerRoute);
 app.listen(5000, () => {
     console.log("Server is running on port 5000 :)");
 
