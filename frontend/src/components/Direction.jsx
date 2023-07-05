263cb60f62af2a8bf9b15394417abe595e284767
Steve9452 <00380119@uca.edu.sv>
2023-07-04 23:31:44 -0600

    Feat: Walking or driving selector

--- frontend/src/components/Direction.jsx
+++ frontend/src/components/Direction.jsx
@@ -8,14 +8,15 @@ export default function Direction ({ feature, coords,to,  ...props}){
 
     const [ direction, setDirection] = useState({})
     const [ steps, setSteps ] = useState([])
-    
+    const [ vehicle, setVehicle ] = useState('Caminando')
+    const [ vehicleRoute, setVehicleRoute ] = useState('walking')
     
     //"https://api.mapbox.com/directions/v5/mapbox/walking/-89.14655,13.698851;-89.14497149275024,13.701844910380359?steps=true&language=es&overview=full&geometries=geojson&access_token=pk.eyJ1IjoibmF0c29scDc3IiwiYSI6ImNsaHF5ejBwYTBkajgzZG1yem02cXI2NW8ifQ.H2s0rN7AbaF2N2kRXWEkxA"
     
     useEffect(() => {
         const from = (coords.lng || -89.14655) + "," + (coords.lat || 13.698851)
         const to2 = (to.lng || -89.23624) + "," + (to.lat || 13.68023)
-        fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${from};${to2}?steps=true&language=es&overview=full&geometries=geojson&access_token=pk.eyJ1IjoibmF0c29scDc3IiwiYSI6ImNsaHF5ejBwYTBkajgzZG1yem02cXI2NW8ifQ.H2s0rN7AbaF2N2kRXWEkxA`).then
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/${vehicleRoute}/${from};${to2}?steps=true&language=es&overview=full&geometries=geojson&access_token=pk.eyJ1IjoibmF0c29scDc3IiwiYSI6ImNsaHF5ejBwYTBkajgzZG1yem02cXI2NW8ifQ.H2s0rN7AbaF2N2kRXWEkxA`).then
         (response => response.json()).then(data => {
             //console.log(data)
 
@@ -42,14 +43,45 @@ export default function Direction ({ feature, coords,to,  ...props}){
                 //console.log(geojson)
             setDirection(geojson)
         })
-  }, [coords, to])
+  }, [coords, to, vehicleRoute])
 
+    const changeVehicle = () => {
+        if(vehicle === 'Caminando'){
+            setVehicle('En vehiculo')
+            setVehicleRoute('driving')
+        }else{
+            setVehicle('Caminando')
+            setVehicleRoute('walking')
+        }
+    }
 
     return(
         <>
         
         <div className="container" >
             <h1 >Como llegar</h1>
+            
+            <div className="routeController">
+            <b>{vehicle}</b>
+
+            <div className="switchContainer">
+            <label className="switch">
+                <input type="checkbox" id="togBtn" onChange={changeVehicle}/>
+                <div className="slider round">
+                </div>
+            </label>
+            {vehicle === 'Caminando' ?
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/walking.png" alt="walking" />
+                :
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/car.png" alt="car" />
+            }
+            </div>
+            
+
+
+            
+
+            </div>
             {
                 steps.length > 0 &&
                 steps.map((step,index) => 
